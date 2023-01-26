ddb81366463bcd947243c173cf63769126874d9c
Jason Wang <wanghung07@gmail.com>
2023-01-26 13:12:52 +0800

    day1

--- calorie-counting/src/main.rs
+++ calorie-counting/src/main.rs
@@ -1,3 +1,18 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let content = fs::read_to_string("./input.txt").expect("failure opening file");
+
+    let mut counts = content
+        .split("\n\n")
+        .map(|chunk| -> usize { chunk.split("\n").map(|row| row.parse().unwrap_or(0)).sum() })
+        .collect::<Vec<_>>();
+
+    // let mut v = counts.collect::<Vec<_>>();
+
+    counts.sort();
+
+    let last_idx = counts.len() - 1;
+
+    println!("The highest count is {}", counts[last_idx]);
 }
